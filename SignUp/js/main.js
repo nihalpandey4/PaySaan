e3883512c5fe81f1b36543ebf208af29c707ee9e
nihalpandey4 <61798365+nihalpandey4@users.noreply.github.com>
2020-03-30 15:47:33 +0530

    signup added

---
+++ SignUp/js/main.js
@@ -0,0 +1,38 @@
+const inputs = document.querySelectorAll(".input");
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function () {
+    realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function () {
+    if (realFileBtn.value) {
+        customTxt.innerHTML = realFileBtn.value.match(
+            /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
+        customBtn.innerHTML="CHOOSE ANOTHER FILE"
+    } else {
+        customTxt.innerHTML = "Upload a document for address proof.";
+    }
+});
+
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+});
